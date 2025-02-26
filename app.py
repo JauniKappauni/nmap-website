db0a0c2bd78dd8362ee7fa38ee3a6a259d77d6cd
JauniKappauni <social.jauni@gmail.com>
2025-02-26 17:38:04 +0100

    Imported request variable from flask Imported module nmap Created a instance of the nmap PortScanner Added GET and POST method to the "/" route Added empty variable output Added statement to prevent code malfunctioning Added retrieval of user input out of html form Added scan of user input(domain) Added iteration of all found hosts Added host to variable output Added state of host to variable output Added iteration of all protocols, which were found Added allocation of all ports found in the protocol to variable ports Added protocol to variable output Added iteration through all opened ports Added Port with it's state to the variable output Added return of output variable to html file Made flask server public

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import nmap
 
 app = Flask(__name__)
 
-@app.route("/")
+scanner = nmap.PortScanner()
+
+@app.route("/", methods=["GET", "POST"])
 def function1():
-    return render_template("index.html")
+    output = ""
+    if request.method == "POST":
+        target = request.form.get("domain/ip-address")
+        scanner.scan(target)
+        for host in scanner.all_hosts():
+            output += f"Host: {host}\n"
+            output += f"State: {scanner[host].state()}\n"
+            for proto in scanner[host].all_protocols():
+                ports = scanner[host][proto].keys()
+                output += f"Protocol: {scanner[host][proto].keys()}\n"
+                for port in ports:
+                    output += f"Port: {port}, State: {scanner[host][proto][port]['state']}\n"
+    return render_template("index.html", output=output)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
